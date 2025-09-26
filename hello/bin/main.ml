bf3970859cf592a64c58a98be2939124fb0172d9
Bruno Scherer <bruno.raphael96@gmail.com>
2025-09-26 00:03:19 -0700

    using modules

--- hello/bin/main.ml
+++ hello/bin/main.ml
@@ -1,2 +1,6 @@
 let () = Printf.printf "%s\n" Hello.Es.v
 let () = Printf.printf "%s\n" Hello.En.v
+
+let exp1 = Sexplib.Sexp.of_string "(This (is an) (s expression))"
+
+let () = Printf.printf "%s\n" (Sexplib.Sexp.to_string exp1)
